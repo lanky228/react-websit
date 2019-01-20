61379c6243fe17f78ee1c9e895d0d2de0bb83f08
liuzhengri <1289206629@qq.com>
2019-01-20 23:48:13 +0800

    增加 图片 card

--- index.js
+++ index.js
@@ -6,12 +6,37 @@ import { Table } from 'antd';
 import { MyTable } from "./js/MyTable.js";
 import { GCDate } from "./js/MyTable.js";
 import { Tabs } from 'antd';
+import { Carousel } from 'antd';
+import { Card, Col, Row } from 'antd';
+
 const TabPane = Tabs.TabPane;
 
 
 function App() {
     const tabPanes = urlList.map(function (item) {
-        return <TabPane tab={item.name} key={item.name}> < MyTable url={item.url} /></TabPane>
+        var tab = null;
+        if (item.type == 'table') {
+            tab = (
+                < MyTable url={item.url} />
+            );
+        } else if (item.type == 'image') {
+            var rows = item.list.map(function (imageItem) {
+                return (
+                    <Row style={{ padding: '10px 20px' }}>
+                        <Card title={imageItem.name}><img src={imageItem.url} alt={imageItem.name + ' : ' + imageItem.url} /></Card>
+                    </Row>
+                );
+            });
+            tab = (
+                <div style={{ background: '#ECECEC' }}>
+                    {rows}
+                </div>
+            );
+        }
+        return (
+            <TabPane tab={item.name} key={item.name}> {tab}</TabPane>
+        );
+
     });
 
     return (
